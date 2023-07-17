e85b3cb2d2b307e531c3d705cc937f1727c84f76
«callmelisabeth» <behoungban@gmail.com>
2023-07-17 18:35:37 +0200

    header

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,14 @@
+<?php
+namespace App\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+class HomeController extends AbstractController
+{
+  #[Route('/', 'home.index', methods:['GET'])]
+  public function number() : Response
+  {
+	$number = rand(0, 100);
+	return $this->render('home.html.twig');
+  }
+}
